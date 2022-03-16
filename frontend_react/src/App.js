95abda261a3f206861f393e291350762d68ea5c4
Sudip Shrestha <xthazudp@gmail.com>
2022-03-16 13:52:48 +0545

    Final initial setup in frontend

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
-import {
-  About,
-  Header,
-  Footer,
-  Skills,
-  Testimonial,
-  Work,
-} from './container/index';
+import { About, Header, Footer, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
 
 const App = () => {
   return (
     <div className='app'>
+      <Navbar />
       <Header />
       <About />
       <Work />
